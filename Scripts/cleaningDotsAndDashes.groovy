2cd9e3308fe3679941234f1465e49e581c4378f1
mecathcart <mdotedot@udel.edu>
2011-12-20 15:49:00 -0500

    refactoring to bring in dictionary lookup repo

---
+++ Scripts/cleaningDotsAndDashes.groovy
@@ -0,0 +1,47 @@
+
+//----------------------------------------------------------------------------------
+// test values (change for your os and directories)
+inputPath='./../GoldStandardFinnishData/RoughData.csv'
+outPath='./../GoldStandardFinnishData/SegmentedData.txt'
+
+
+InputStreamReader sourcefile = new InputStreamReader(new FileInputStream(inputPath),"UTF-8");
+
+numberToStopTheLoopToShowOnlyPartOfIt = 0
+def flagStop = 0
+def fmorphemes = [:]
+
+
+while ( flagStop !=1 && (line = sourcefile.readLine() ) != -1){ 
+    numberToStopTheLoopToShowOnlyPartOfIt ++                   //(to on    ly run part of the loop)    
+    if(numberToStopTheLoopToShowOnlyPartOfIt >5 || line == null){ break; }    //(to only run part of the loop)
+           
+   if(line ==null){
+       line = ""
+       println "Flagging stop"
+       flagStop = 1        
+    }
+    
+
+   /* 
+   First if is to ensure code is working, second if runs code if code is working.
+   */
+   if (line.length() > 1){
+   
+       def fields = line.split(",")
+ 
+   
+       def fgloss = fields[1].split("-")
+   
+   
+       println fgloss
+   
+   
+   
+   
+   
+   
+    }
+}
+
+//println fmorphemes
